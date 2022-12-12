de78e981dbe855f897d0e5f20730c69a2f05738b
Việt Hùng <viet.hung.2898@gmail.com>
2022-12-12 07:48:26 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,9 @@ import ThreeCanvas from './components/three'
 import './index.css'
 
 function App() {
-  const [show, setShow] = useState(true)
-
-  const handleClick = (e: React.MouseEvent) => {
-    setShow(!show)
-  }
-
   return (
     <div className="">
-      { show ? 
-        <ThreeCanvas /> 
-      : null }
-      <button onClick={handleClick}>{show ? 'Hide' : 'Show'}</button>
+      <ThreeCanvas /> 
     </div>
   )
 }
